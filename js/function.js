da89204339edce6f26783cceaaefde3dabd8c5f2
BarriosPatricia <patribarrios980@gmail.com>
2023-01-31 21:02:35 -0300

    Se agrega el proyecto

---
+++ js/function.js
@@ -0,0 +1,10 @@
+
+  document.getElementById('boton').onclick= function() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "https://www.clarin.com/img/2021/06/17/BXuQ_UHdn_340x340__1.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+  }
+  
